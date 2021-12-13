6a37a35d9f84178ab50870eafd88831ffae3d8c7
Ali Nazif Koca <nazif.koca@ogr.sakarya.edu.tr>
2021-12-13 09:31:23 -0800

    added cd

--- src/main.c
+++ src/main.c
@@ -2,10 +2,28 @@
 #include <string.h>
 #include <unistd.h>
 #include "colors.h"
+#include <stdlib.h>
+
+char workingDir[80];
+
+void cd(const char* path)
+{
+    int err = chdir(path);
+    if(err)
+    {
+        setColor(COLOR_RED);
+        printf("Hata: %s", path);
+        printf("Hata: Komut icra edilemiyor.\n");
+    }
+    
+    getcwd(workingDir, sizeof(workingDir));
+    printf("Current Dir = %s\n", workingDir);
+    setenv("pwd", workingDir, 1);
+}
+
 int main()
 {
-    char command[80];
-    char workingDir[80];
+    char input[80];
     if(getcwd(workingDir, sizeof(workingDir)))
     {
         //setColor(COLOR_WHITE);
@@ -20,14 +38,34 @@ int main()
 
     do
     {
+        //memset(input,0,strlen(input));
         setColor(COLOR_GREEN);
         printf("%s/: ", workingDir);
         setColor(COLOR_BLUE);
         printf("sau > ");
         resetColor();
-        scanf("%s", command);
-        //printf("%s\n", command);
-    } while (strcmp(command, "exit"));
+        //scanf("%[^\n]", input);
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0;
+        printf("You entered the command: %s\n", input);
+        char* command = strtok(input, " ");
+        char* token = strtok(NULL, " ");
+        if(!strcmp(command, "cd"))
+        {
+            if(!token)
+            {
+                setColor(COLOR_RED);
+                printf("Hata: Komutun icra edilmesi icin yeteri sayida parametre yok.\n");
+                return 0;
+            }
+            else
+            {
+                cd(token);
+            }
+        }
+        
+    } while (strcmp(input, "exit"));
+    
     
     
     
